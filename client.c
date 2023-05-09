32008da815838b93127c20427a1cc24ba2efc9a8
Jens Kulka <kulkajens82@gmail.com>
2023-05-09 11:15:47 +0200

    feat(client.c): add client implementation for sending messages to server feat(server.c): add server implementation for receiving messages from client

---
+++ client.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 100
+
+void sig_handler(int sig_num) {
+    if (sig_num == SIGUSR1) {
+        // Do nothing - this bit is a 1
+    } else if (sig_num == SIGUSR2) {
+        // Terminate the message loop - this bit is a 0
+        exit(0);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s <server_pid> <message>\n", argv[0]);
+        exit(1);
+    }
+
+    int server_pid = atoi(argv[1]);
+    char *message = argv[2];
+    int message_length = strlen(message);
+
+    printf("Sending message '%s' to server with PID %d\n", message, server_pid);
+
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = sig_handler;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    for (int i = 0; i < message_length; i++) {
+        char c = message[i];
+        for (int j = 0; j < 8; j++) {
+            if (c & (1 << j)) {
+                kill(server_pid, SIGUSR1);
+            } else {
+                kill(server_pid, SIGUSR2);
+            }
+            usleep(1000); // Add a delay of 1 millisecond
+        }
+    }
+
+    // Signal end of message with 8 SIGUSR2 signals
+    for (int i = 0; i < 8; i++) {
+        kill(server_pid, SIGUSR2);
+        usleep(1000); // Add a delay of 1 millisecond
+    }
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,82 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <signal.h>
+// #include <string.h>
+
+// #define MAX_STRING_LENGTH 100
+
+// void sig_handler(int sig_num) {
+//     if (sig_num == SIGUSR1) {
+//         printf("Server received SIGUSR1 signal\n");
+//     } else if (sig_num == SIGUSR2) {
+//         printf("Server received SIGUSR2 signal\n");
+//     }
+// }
+
+// int main() {
+//     printf("Server PID: %d\n", getpid());
+
+//     struct sigaction sa;
+//     memset(&sa, 0, sizeof(sa));
+//     sa.sa_handler = sig_handler;
+//     sigaction(SIGUSR1, &sa, NULL);
+//     sigaction(SIGUSR2, &sa, NULL);
+
+//     while (1) {
+//         pause();
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 100
+
+void sig_handler(int sig_num, siginfo_t *sig_info, void *context) {
+    static int bit_count = 0;
+    static char current_byte = 0;
+    static char received_string[MAX_STRING_LENGTH];
+
+    if (sig_num == SIGUSR1) {
+        current_byte |= (1 << bit_count);
+    }
+
+    bit_count++;
+
+    if (bit_count == 8) {
+        if (current_byte == '\0') {
+            printf("Received string: %s\n", received_string);
+            // printf("Received signal %d\n", sig_num);
+            bit_count = 0;
+            current_byte = 0;
+            memset(received_string, 0, MAX_STRING_LENGTH);
+        } else {
+            strncat(received_string, &current_byte, 1);
+            bit_count = 0;
+            current_byte = 0;
+        }
+    }
+}
+
+int main() {
+    printf("Server PID: %d\n", getpid());
+
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_sigaction = sig_handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    while (1) {
+        pause();
+    }
+
+    return 0;
+}

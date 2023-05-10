5a436f822caabc83823ed276cb18e61e5bb88844
Jens Kulka <kulkajens82@gmail.com>
2023-05-10 11:05:38 +0200

    chore(makefile): remove makefile feat(minitalk.h): add minitalk header file with necessary includes and defines

---
+++ minitalk.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minilib.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 10:55:10 by jkulka            #+#    #+#             */
+/*   Updated: 2023/05/10 10:56:23 by jkulka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK
+#	define MINITALK
+
+#	include "libft/src/libft.h"
+#	include <stdlib.h>
+#	include <unistd.h>
+#	include <signal.h>
+#	include <string.h>
+
+#endif
+
+
